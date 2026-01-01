54f932cdf7b96365a2e625ae03cba30d25134aca
Ryohei Ueda <garaemon@gmail.com>
2025-12-31 17:56:57 -0800

    Fix lint errors and warnings

--- src/lib/url-state.test.ts
+++ src/lib/url-state.test.ts
@@ -46,7 +46,6 @@ describe('url-state', () => {
   });
 
   it('should prioritize LZ-compressed content over "text" parameter', () => {
-    const params = new URLSearchParams();
     // LZ compressed 'Compressed Content' (roughly)
     // We rely on encodeState to get valid compressed string for test
     const compressedState = encodeState({ ...defaultState, content: 'Compressed Content' });
